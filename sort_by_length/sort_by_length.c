fa28d4d569c75eb60358b42925ebee94f69a8f23
Douglas Alves <rachebartm00s@proton.me>
2025-01-26 13:28:31 +0000

    kata passed

--- sort_by_length/sort_by_length.c
+++ sort_by_length/sort_by_length.c
@@ -2,37 +2,46 @@
 #include <stdio.h>
 #include <string.h>
 
-void	counting_sort(int *arr, int size)
+int	comp(const void *a, const void *b)
 {
-	int	output[size];
-
+	return (*(int *)a - *(int *)b);
 }
 
-//function must sort a given string array by the length of the strings
 void	sort_by_length(const char *str[])
 {
 	int	*length_index = calloc(5, sizeof(int));
 	int	size = 0;
+	int	i = 0;
 
 	for (size = 0; str[size] != NULL; size++)
 	{
 		length_index[size] = strlen(str[size]);
 	}
-	for (int i = 0; str[i] != NULL; i++)
+	qsort(length_index, size, sizeof(int), comp);
+	while (i < size)
 	{
-		printf("%d\n", length_index[i]);
+		int	j = 0;
+		while (j < size)
+		{
+			if (strlen(str[j]) == length_index[i])
+			{
+				const char	*tmp = str[i];
+				str[i] = str[j];
+				str[j] = tmp;
+			}
+			j++;
+		}
+		i++;
 	}
 }
-//i have a int *arr with the length of unsorted array.
-//need to sort the array, and create a mirrored array of string with the lengths.
-//10 7 4 8 9 becomes a array of strings with sizes: 4 7 8 9 10
-// now we take from the 'lenght index' we copy the 0th array,which has the lenght 10
-//and put at the corresponding index in sorted array, in this case index 4.
-// logic similar to counting sort, discover how to do it.
 
 int	main(void)
 {
 	const char	*str[] = {"Telescopes", "Glasses", "Eyes", "Monocles", "Cyberpunk", NULL};
 
 	sort_by_length(str);
+	for (int i = 0; str[i] != NULL; i++)
+	{
+		printf("%s\n", str[i]);
+	}
 }
